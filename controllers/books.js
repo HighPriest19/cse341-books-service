fbf3f89eb8d68420340d10e799f8da19f9a76eb4
HighPriest19 <kryptonolagunju@gmail.com>
2026-09-01 20:36:42 +0100

    commit

--- controllers/books.js
+++ controllers/books.js
@@ -1,31 +1,85 @@
 const mongodb = require('../db/connect');
 const { ObjectId } = require('mongodb');
 
-// GET /books - Fetch all books
+// GET /books
 const getAll = async (req, res) => {
   try {
-    const result = await mongodb.getDb().collection('books').find();
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
+    const lists = await mongodb.getDb().collection('books').find().toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
 
-// GET /books/:id - Fetch single book by ID
+// GET /books/:id
 const getSingle = async (req, res) => {
   try {
     const bookId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().collection('books').find({ _id: bookId });
-    result.toArray().then((lists) => {
-      if (lists.length === 0) {
-        return res.status(404).json({ message: 'Book not found' });
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists[0]);
-    });
+    const lists = await mongodb.getDb().collection('books').find({ _id: bookId }).toArray();
+    if (lists.length === 0) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid Book ID format' });
+  }
+};
+
+// POST /books
+const createBook = async (req, res) => {
+  try {
+    const book = {
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      publishedYear: req.body.publishedYear,
+      isbn: req.body.isbn
+    };
+    const response = await mongodb.getDb().collection('books').insertOne(book);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json({ message: 'Failed to create book' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// PUT /books/:id
+const updateBook = async (req, res) => {
+  try {
+    const bookId = new ObjectId(req.params.id);
+    const book = {
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      publishedYear: req.body.publishedYear,
+      isbn: req.body.isbn
+    };
+    const response = await mongodb.getDb().collection('books').replaceOne({ _id: bookId }, book);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'Book not found or no changes made' });
+    }
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid Book ID format' });
+  }
+};
+
+// DELETE /books/:id
+const deleteBook = async (req, res) => {
+  try {
+    const bookId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().collection('books').deleteOne({ _id: bookId });
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'Book not found' });
+    }
   } catch (err) {
     res.status(400).json({ message: 'Invalid Book ID format' });
   }
@@ -33,5 +87,8 @@ const getSingle = async (req, res) => {
 
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  createBook,
+  updateBook,
+  deleteBook
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('Books Web Service API is running');
+});
+
 router.use('/books', require('./books'));
 
 module.exports = router;
